7a17e7d5cedd7d42c316eb201102d473b57fd90b
NguyenMTrang <minhtrang_92@yahoo.com>
2018-12-05 17:01:11 -0800

    Added some stuffs

--- Chapter 2/Data Cleaning.py
+++ Chapter 2/Data Cleaning.py
@@ -11,13 +11,14 @@ import pandas as pd
 fullpath = 'D:/GitHub/PredictiveAnalysis/Chapter 2/'
 filename = 'titanic3.csv'
 data = pd.read_csv(fullpath + filename)
+data.to_csv('cleansed.csv',sep =',')
 
 print(data.head(10))
 print(data.shape)
 data_columns = data.columns.values
 data_stats = data.describe()
 data.dtypes
-
+data.info()
 
 #%% Handling Missing Data.
 data.isnull()
@@ -39,16 +40,21 @@ data['age'].fillna(data['age'].mean())
 data['age'].fillna(method = 'ffill')
 data['age'].fillna(method = 'backfill')
 
+# Droping dublicates: duplicated(), drop_duplicates()
+data.drop_duplicates()
+# Alter the columns with index: set_Index()
+
 #%% Creating dummy variables.
 dummy_sex = pd.get_dummies(data['sex'])
 data.drop('sex',axis = 1) # data.drop('age',1,inplace = true) drop columns without reassign data.
 data.drop(data.columns[[0,1]],axis = 1)
 data.drop(['sex','body'],axis = 1)
+data.drop(data.columns[[0]],axis = 1, inplace = True)
 
 
 data_dummy = data.drop('sex',axis = 1)
 data_dummy.join(dummy_sex)
-
+data['Sex'] = data['Sex'].map({'male':0,'female':1})
 #%% Visualizing a dataset by basic plotting
 
 import pandas as pd
--- Chapter 3/Data Wrangling.py
+++ Chapter 3/Data Wrangling.py
@@ -7,12 +7,16 @@ Data Wrangling
 """
 #%% Subsetting a dataset:
 import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
 
-#fullpath = 'D:/GitHub/PredictiveAnalysis/Chapter 3/'
-fullpath = '/Users/Sony/Desktop/GitHub/PredictiveAnalysis/Chapter 3/'
+fullpath = 'D:/GitHub/PredictiveAnalysis/Chapter 3/'
+#fullpath = '/Users/Sony/Desktop/GitHub/PredictiveAnalysis/Chapter 3/'
 
 filename = 'Customer Churn Model.txt'
 data = pd.read_csv(fullpath + filename)
+
 
 # Selecting columns:
 account_length = data['Account Length']
@@ -25,7 +29,7 @@ wanted_columns = ['Account Length','VMail Message','Day Calls']
 subdata = data[wanted_columns]
 subdata.head()
 
-data.columns.values.tolist()
+
 
 # Selecting rows:
 data[0:50]
@@ -37,22 +41,31 @@ CA_data = data[(data['State']== 'CA') & (data['Day Calls'] > 100)]
 CA_data = data[(data['State']== 'CA') | (data['Day Calls'] > 100)]
 CA_data.shape
 
-# Selecting both rows and columns - see loc and iloc.
+# Selecting both rows and columns - loc and iloc are recommended to use.
     
 subdata = data[['Account Length','VMail Message','Day Calls']][:50] #first 50 records 
-data.loc[:50]
+
 data.ix[1:100,1:5]
 data.ix[1:100,[0,1,2]]
+
+data.iloc[:5] # return the first 5 rows
+data.iloc[:,1:3]
+data.iloc[:, [1,2,5]] #gets rows (or columns) at particular positions in the index (so it only takes integers).
+#data.iloc[:data.index.get_loc('Row_Index_label') + 1, :4]
+data.loc[:5] # return the first 6 rows - treat 5 as the row index.
+data.loc[:,['Account Length' ,'Day Calls']] # gets rows (or columns) with particular labels from the index
+data.loc[:,'Account Length' :'Day Calls']
+data.loc[data['State'] == 'CA']
+
+
 
 # Creating a new column:
 data['Total Mins'] = data['Day Mins'] + data['Night Mins'] + data['Eve Mins'] + data['Intl Mins'] 
 data['Total Mins'].head()
 
 #%% Generating random numbers and their usage
-import numpy as np
 np.random.randint(1,100)
 np.random.random() # range 0 - 1
-import random
 random.randrange(0,100,5)
 
 a = range(100)
@@ -67,17 +80,40 @@ for i in range(5):
 # PROBABILITY DENSITY FUNCTION
 # CUMULATIVE DENSITY FUNCTION
 # UNIFORM DISTRIBUTION
-import numpy as np
+
 randnum=np.random.uniform(1,100,100)
 
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 a=np.random.uniform(1,100,100000) # increase size to see normal distribution.
 plt.hist(a)
 
 # NORMAL DISTRIBUTION:
-a=np.random.randn(100)
+a=np.random.randn(100000)
 plt.hist(a)
 a.mean()
+
+# Generating a dummy data frame:
+d = pd.DataFrame({'A':np.random.randn(10),'B':2.5*np.random.randn(10) + 1.5})
+plt.hist(d['A'])
+
+
+column_list = data.columns.values.tolist()
+a = len(column_list)
+np.random.seed(1)
+d = pd.DataFrame({'Column_Name': column_list,'A':np.random.randn(a),'B':2.5*np.random.randn(a) + 1.5})
+
+# Index can also be passed as parameters
+d=pd.DataFrame({'A':np.random.randn(10),'B':2.5*np.random.randn(10)+1.5},index=range(10,20))
+
+#%% Grouping the data - aggregation, filtering and transformation.
+d.drop(d.columns[[0]],axis = 1, inplace = True)
+
+data_group = data.groupby(['State','Area Code'])['Phone'].count()
+data.info()
+
+#%% FIT ==> PREDICT ==> EVALUATE (accuracy 65 - 90% good)
+# First choice: the supposted vector learning Machine model
+# Then, gradent descent.
+
 
